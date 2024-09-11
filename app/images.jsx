4b60bf23b90e4f934fbb799b80fb05c8f1132492
28sungmin <gooddays4001@naver.com>
2024-09-11 11:04:23 +0900

    first commit

---
+++ app/images.jsx
@@ -0,0 +1,11 @@
+import CheckBoxOutline from "../assets/images/icons/check_box_outline.png";
+import CheckBox from "../assets/images/icons/check_box.png";
+import DeleteForever from "../assets/images/icons/delete_forever.png";
+import Edit from "../assets/images/icons/edit.png";
+
+export const images = {
+  uncompleted: CheckBoxOutline,
+  completed: CheckBox,
+  delete: DeleteForever,
+  update: Edit,
+};
